dc62426916b2276656781a487447a0066c0f28c6
Eric-Mai-25 <ericmai2@gmail.com>
2023-10-22 22:08:37 -0700

    login signup finished

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,12 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import session from './session.js';
+import tweets from './tweets.js'
+import { sessionErrorsReducer } from './session.js';
 
 const rootReducer = combineReducers({
-  
+  session,
+    tweets
 });
 
 let enhancer;
